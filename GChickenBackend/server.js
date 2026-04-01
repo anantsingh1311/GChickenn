f343379d367a805b205351d66a4bc2985a1618ca
anantsingh1311 <62670882+anantsingh1311@users.noreply.github.com>
2026-04-01 11:47:55 +0100

    updated bug

--- GChickenBackend/server.js
+++ GChickenBackend/server.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: ["http://localhost:3000","https://g-chickenn.vercel.app/"],
+    origin: ["http://localhost:3000","https://g-chickenn.vercel.app"],
     credentials: true
   })
 );
